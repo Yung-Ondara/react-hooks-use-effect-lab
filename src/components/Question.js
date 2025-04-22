4b6427d22607a9ae2d7fb9a2064b8234f053104b
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-04-22 18:05:54 +0300

    added time deliverables using useEffect

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining((prev) => {
+        if (prev <= 1) {
+          clearInterval(timer);
+          onAnswered(false);
+          return 10; // Reset to 10 seconds
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer); // Cleanup the interval on unmount
+  }, [onAnswered]);
+
+ useEffect(() => {
+  const timeoutld = setTimeout(() => {
+     onAnswered();
+  }, 10000); // 10 seconds timeout
+  return () => clearTimeout(timeoutld); // Cleanup the timeout on unmount
+}, [onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
